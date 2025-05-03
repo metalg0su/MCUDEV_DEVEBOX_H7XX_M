655f816228c2d71be6dacd92b39fef6a67fce1b6
metalg0su <metalg0su@gmail.com>
2025-05-03 21:57:51 +0900

    nucleo 보드 참고: 마참내!

--- mpconfigboard.mk
+++ mpconfigboard.mk
@@ -11,9 +11,8 @@ AF_FILE = boards/stm32h743_af.csv
 #endif
 
 # When not using Mboot the ISR text goes first, then the rest after the filesystem
-LD_FILES = boards/stm32h743.ld boards/common_ifs.ld
+LD_FILES = boards/stm32h743.ld boards/common_basic.ld
 TEXT0_ADDR = 0x08000000
-TEXT1_ADDR = 0x08040000
 
 # MicroPython settings
 MICROPY_PY_LWIP = 1
